e79379812677420f61f2a2ad0385b06402402a7f
Wes Mehaffy <wesonweb@gmail.com>
2023-01-17 19:11:09 +0000

    route controller and view for ask and answer

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,5 @@
+class QuestionsController < ApplicationController
+  def ask; end
+
+  def answer; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # the questions
+  get '/ask', to: 'questions#ask'
+
+  # the answers
+  get '/answer', to: 'questions#answer'
 end
